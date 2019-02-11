9e7de6e730fcae7c04f35bf69e0f6f66b9a64e10
RanjitaPandit <ranjitapandit@gmail.com>
2019-02-11 18:04:50 +1100

    Mars Framework Version 1.0
    Added Share Skills Page

---
+++ Mars-New/MarsFramework/MarsFramework/Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsFramework.Pages;
+
+
+namespace MarsFramework
+{
+    public class Program
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class Tenant : Global.Base
+        {
+
+            [Test]
+            public void UserAccount()
+            {
+                // Creates a toggle for the given test, adds all log events under it    
+                test = extent.StartTest("Search for a Property");
+
+                // Create an class and object to call the method
+                //  Profile obj = new Profile();
+                // obj.EditProfile();
+
+
+                //Add a class to call the Method of Share Skills
+                ShareSkills obj = new ShareSkills();
+                obj.AddShareSkills();
+
+            }
+         
+        }
+    }
+}
